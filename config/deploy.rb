daae7351215ef8c9af5ce90758b4d04788fe9a06
Ben Hoad <ben@benhoad.net>
2014-01-21 12:16:38 +1000

    testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ after 'deploy:update_code' do
   run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   
   # Compile Assets
+  run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:clean"
   run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
 end
 
