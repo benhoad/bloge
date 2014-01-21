a54701f05cf674bd6d63fc14999201f94ff0c8d2
Ben Hoad <ben@benhoad.net>
2014-01-21 10:40:54 +1000

    switched database

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ role :db,  "192.34.59.191", :primary => true
 set :user, "root"
 set :runner,                     "www-data"
 set :admin_runner,               "www-data"
+
 set :scm,                        :git
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
@@ -22,3 +23,14 @@ set :deploy_via, :copy
 
 set :normalize_asset_timestamps, false
 set :rvm_type, :system
+
+deploy.task :restart, roles: :app do
+  # Fix Permissions
+  sudo "chown -R www-data:www-data #{current_path}"
+  sudo "chown -R www-data:www-data #{latest_release}"
+  sudo "chown -R www-data:www-data #{shared_path}/bundle"
+  sudo "chown -R www-data:www-data #{shared_path}/log"
+  
+  # Restart Application
+  run "service unicorn restart"
+end
